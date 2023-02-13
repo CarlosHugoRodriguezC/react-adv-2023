639ad7e0c19daf25e0d9c42201f9e1f8565282b4
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2023-02-12 18:25:48 -0600

    nested routes with lazy load

---
+++ react-app/src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,38 @@
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../pages';
+
+export const LazyLayout = () => {
+  return (
+    <div>
+      <h1>LazyLayout</h1>
+      {/* Children routes will be here */}
+      <ul>
+        <li>
+          <NavLink to='lazy1'>Lazy1</NavLink>
+        </li>
+        <li>
+          <NavLink to='lazy2'>Lazy1</NavLink>
+        </li>
+        <li>
+          <NavLink to='lazy3'>Lazy1</NavLink>
+        </li>
+      </ul>
+      <Routes>
+        <Route path='lazy1' element={<LazyPage1 />} />
+        <Route path='lazy2' element={<LazyPage2 />} />
+        <Route path='lazy3' element={<LazyPage3 />} />
+        {/* <Route
+          path='*'
+          element={
+            <div>
+              <h1>Not Found</h1>
+            </div>
+          }
+        /> */}
+        <Route path='*' element={<Navigate to='lazy1' replace />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default LazyLayout;
